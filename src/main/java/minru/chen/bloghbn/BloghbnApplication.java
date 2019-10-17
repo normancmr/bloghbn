ae07ec9e3e6b67d2093e7bbad842634af4d50500
minru <normancmr@gmail.com>
2019-10-17 14:50:59 -0400

    Initial commit

---
+++ src/main/java/minru/chen/bloghbn/BloghbnApplication.java
@@ -0,0 +1,13 @@
+package minru.chen.bloghbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloghbnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloghbnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/minru/chen/bloghbn/BloghbnApplicationTests.java
@@ -0,0 +1,13 @@
+package minru.chen.bloghbn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloghbnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
